1fc0ef0ff69de8529d5d4b94f01c3bb33d1c9295
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-07-22 11:19:42 +0700

    feat: add create database sql

---
+++ sql/create.sql
@@ -0,0 +1,36 @@
+-- postgreSQL
+-- Data
+
+CREATE TABLE "Dosen" (
+  "NIK" varchar(20) PRIMARY KEY,
+  "Nama" varchar(100) NOT NULL,
+  "TanggalLahir" timestamp NOT NULL
+);
+
+CREATE TABLE "MataKuliah" (
+  "Kode" varchar(10) PRIMARY KEY,
+  "NIKDosen" varchar(20),
+  "Nama" varchar(100) NOT NULL,
+  "SKS" smallint NOT NULL
+);
+
+CREATE TABLE "Nilai" (
+  "KodeMK" varchar(10),
+  "NoMhs" varchar(10),
+  "Nilai" float NOT NULL
+);
+
+CREATE TABLE "Mahasiswa" (
+  "NoMhs" varchar(20) PRIMARY KEY,
+  "Nama" varchar(100) NOT NULL,
+  "TanggalLahir" timestamp NOT NULL,
+  "TanggalMasuk" timestamp
+);
+
+CREATE UNIQUE INDEX ON "Nilai" ("KodeMK", "NoMhs");
+
+ALTER TABLE "MataKuliah" ADD FOREIGN KEY ("NIKDosen") REFERENCES "Dosen" ("NIK");
+
+ALTER TABLE "Nilai" ADD FOREIGN KEY ("KodeMK") REFERENCES "MataKuliah" ("Kode");
+
+ALTER TABLE "Nilai" ADD FOREIGN KEY ("NoMhs") REFERENCES "Mahasiswa" ("NoMhs");
